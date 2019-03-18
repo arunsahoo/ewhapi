a9bd2aa55a1419680fed8e0c1019c2a28148f109
Arun Kumar Sahoo <sahoo.kumar.arun@gmail.com>
2019-03-18 18:41:18 +0530

    add apikey component to api routes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'EWHAPI'),
 
     /*
     |--------------------------------------------------------------------------
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Passport\PassportServiceProvider::class,
+        Ejarnutowski\LaravelApiKey\Providers\ApiKeyServiceProvider::class,
         /*
          * Application Service Providers...
          */
